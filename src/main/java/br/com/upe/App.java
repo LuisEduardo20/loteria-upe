04d1838492b4e27dfcae9fc5e4ab5282e815ba62
Luis Eduardo Santos Costa <luiseduardo70@live.com>
2021-10-22 10:12:54 -0300

    primeiro commit

---
+++ src/main/java/br/com/upe/App.java
@@ -0,0 +1,13 @@
+package br.com.upe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
