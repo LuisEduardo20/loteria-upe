77a9a1567441c6570e21ff65cc7ce75cae58d5b3
Luis Eduardo Santos Costa <luiseduardo70@live.com>
2021-10-26 19:25:00 -0300

    Passagem do nome do resultado a ser pegado (ainda incompleto)

--- src/main/java/br/com/upe/App.java
+++ src/main/java/br/com/upe/App.java
@@ -4,9 +4,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        String[] resultado = ResultadoMegasena.obtemUltimoResultado();
-        for(String dezena: resultado) {
-        	System.out.print(dezena + " ");
-        }
+        String resultado = ResultadoMegasena.obtemUltimoResultado("mega-sena");
+//        for(String dezena: resultado) {
+//        	System.out.print(dezena + " ");
+//        }
+        System.out.print("mega sena:" + resultado);
     }
 }
