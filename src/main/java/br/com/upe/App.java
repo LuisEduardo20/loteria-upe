dba2f1c1f9cc86870e60aa6b88b903f64cc6e932
Luis Eduardo Santos Costa <luiseduardo70@live.com>
2021-10-27 15:12:24 -0300

    Instancias de objetos para mega-sena quina e time-mania

--- src/main/java/br/com/upe/App.java
+++ src/main/java/br/com/upe/App.java
@@ -1,16 +1,24 @@
 package br.com.upe;
 
+import br.com.upe.ResultadoMegasena;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        String resultado = ResultadoMegasena.obtemUltimoResultado("mega-sena");
-//        for(String dezena: resultado) {
-//        	System.out.print(dezena + " ");
-//        }
-        System.out.print("mega sena:" + resultado);
+    	// Resultado Mega Sena
+    	ResultadoMegasena resultadoMegaSena = new ResultadoMegasena("mega-sena");
+        String resultado = resultadoMegaSena.obtemUltimoResultado();
+        System.out.println("mega sena:" + resultado);
+        
+        // Resultado Quina
+        ResultadoMegasena resultadoQuina = new ResultadoMegasena("quina");
+        String resultado2 = resultadoQuina.obtemUltimoResultado();
+        System.out.println("quina: " + resultado2);
+        
+        // Resultado Time mania
+        ResultadoMegasena resultadoTimemania = new ResultadoMegasena("timemania");
+        String resultado3 = resultadoTimemania.obtemUltimoResultado();
+        System.out.println("timemania: " + resultado3);
     }
 }
-
-
