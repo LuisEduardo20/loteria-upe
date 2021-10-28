0caa24a91e119bab24ab150ad43665b6e03525e9
Will <dsilverfox98@gmail.com>
2021-10-28 08:49:16 -0300

    Formatação do resultado da loteria

--- src/main/java/br/com/upe/App.java
+++ src/main/java/br/com/upe/App.java
@@ -2,24 +2,22 @@ package br.com.upe;
 
 import br.com.upe.ResultadoMegasena;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
 
-    	// Resultado Mega Sena
-    	ResultadoMegasena resultadoMegaSena = new ResultadoMegasena("mega-sena");
+        // Resultado Mega Sena
+        ResultadoMegasena resultadoMegaSena = new ResultadoMegasena("mega-sena");
         String resultado = resultadoMegaSena.obtemUltimoResultado();
-        System.out.println("mega sena:" + resultado);
-        
-        // Resultado Quina
-        ResultadoMegasena resultadoQuina = new ResultadoMegasena("quina");
-        String resultado2 = resultadoQuina.obtemUltimoResultado();
-        System.out.println("quina: " + resultado2);
-        
+        System.out.println("Mega sena:" + resultado);
+
         // Resultado Time mania
         ResultadoMegasena resultadoTimemania = new ResultadoMegasena("timemania");
         String resultado3 = resultadoTimemania.obtemUltimoResultado();
-        System.out.println("timemania: " + resultado3);
+        System.out.println("Timemania:" + resultado3);
+
+        // Resultado Quina
+        ResultadoMegasena resultadoQuina = new ResultadoMegasena("quina");
+        String resultado2 = resultadoQuina.obtemUltimoResultado();
+        System.out.println("Quina:" + resultado2);
     }
 }
--- src/main/java/br/com/upe/ResultadoMegasena.java
+++ src/main/java/br/com/upe/ResultadoMegasena.java
@@ -10,45 +10,50 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import java.io.IOException;
 
 public class ResultadoMegasena {
-	
+
 	private static String MARCA_INICIAL_RETORNO_NAO_UTIL;
-	
+
 	public ResultadoMegasena(String nomeSorteio) {
 
 		this.MARCA_INICIAL_RETORNO_NAO_UTIL = "<ul class=\"resultado-loteria " + nomeSorteio + "\">";
 	}
-	
+
 	private final static String URL = "http://loterias.caixa.gov.br/wps/portal/loterias/landing/";
 	private final static String MARCA_FINAL_RETORNO_NAO_UTIL = "</ul>";
-	
+
 	public static String obtemUltimoResultado() {
 		CloseableHttpClient httpclient = HttpClientBuilder.create().build();
-		
+
 		try {
 			HttpGet httpget = new HttpGet(URL);
-			
+
 			ResponseHandler<String> responseHandler = new BasicResponseHandler();
-			
+
 			String html = httpclient.execute(httpget, responseHandler);
-			
+
 			return obterDezenas(html);
-		}
-		catch(IOException e) {
+		} catch (IOException e) {
 			System.out.println(e);
 			throw new RuntimeException("Um erro inesperado ocorreu.", e);
-		}
-		finally {
+		} finally {
 			httpclient.getConnectionManager().shutdown();
 		}
 	}
-	
+
 	public static String obterDezenas(String html) {
 		Integer parteInicial = html.indexOf(MARCA_INICIAL_RETORNO_NAO_UTIL);
 
 		Integer parteFinal = html.indexOf(MARCA_FINAL_RETORNO_NAO_UTIL, parteInicial);
 
-		String extracao = html.substring(parteInicial, parteFinal).replaceAll("[^0-9]", " ");
+		String extracao = html.substring(parteInicial, parteFinal).replaceAll("[^0-9]", "");
 
-		return extracao;
+		String teste = "";
+		for (int i = 0; i < extracao.length(); i += 2) {
+			teste += " ";
+			teste += extracao.substring(i, i + 2);
+		}
+
+		return teste;
+
 	}
 }
