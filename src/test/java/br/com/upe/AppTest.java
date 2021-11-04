0dd5ed6952821f3d4e10aed3995e230cbbfea2e8
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2021-11-03 22:25:50 -0300

    resolve #2

--- src/test/java/br/com/upe/AppTest.java
+++ src/test/java/br/com/upe/AppTest.java
@@ -1,6 +1,6 @@
 package br.com.upe;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -8,13 +8,32 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+{ 
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( false );
+    public void verificarDezenaMegasena() {
+    	
+    	ResultadoLoteria resultadoMegaSena = new ResultadoLoteria("mega-sena");
+        String resultado = resultadoMegaSena.obtemUltimoResultado();
+    	
+		assertEquals(12, resultado.length());
     }
+    
+    @Test
+    public void verificarDezenaTimemania() {
+    	
+    	ResultadoLoteria resultadoTimemania = new ResultadoLoteria("timemania");
+        String resultado = resultadoTimemania.obtemUltimoResultado();
+    	
+		assertEquals(14, resultado.length());
+    }
+    
+    @Test
+    public void verificarDezenaQuina() {
+    	
+    	ResultadoLoteria resultadoQuina = new ResultadoLoteria("quina");
+        String resultado = resultadoQuina.obtemUltimoResultado();
+    	
+		assertEquals(10, resultado.length());
+    }
+       
 }
